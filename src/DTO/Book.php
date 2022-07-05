bbe76c590cbc72674497de9e6c32c29639ee4c19
77web <info@77-web.com>
2022-07-05 22:21:24 +0900

    mark Book DTO as ApiResource

--- src/DTO/Book.php
+++ src/DTO/Book.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ApiResource]
 class Book
 {
+    public int|null $id = null;
     public string|null $name = null;
     public string|null $authorName = null;
     public int|null $publishedAt = null;
