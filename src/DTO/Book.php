71fef07730cd53e03ef1827f773f00405e130ba5
77web <info@77-web.com>
2022-07-05 22:22:31 +0900

    marked ApiProperty(identifier: true)

--- src/DTO/Book.php
+++ src/DTO/Book.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 
 #[ApiResource]
 class Book
 {
+    #[ApiProperty(identifier: true)]
     public int|null $id = null;
     public string|null $name = null;
     public string|null $authorName = null;
